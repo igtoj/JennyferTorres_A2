5c355cf76051e554d4f19024a08d2463efbcf3a8
igtoj <j.iglandinitorres@gmail.com>
2023-10-08 17:22:37 -0500

    Avance 2

--- R/Activity_A2.R
+++ R/Activity_A2.R
@@ -10,3 +10,15 @@ data <- read.csv(paste0(parentFolder,"/Dataset/diabetes_012.csv"))
 
 data$Diabetes_012 <- ifelse(data$Diabetes_012 == 0, 0, 1)
 
+set.seed(27)
+data_ <- data[sample(nrow(data), 3000), ]
+
+table(data_$Sex)
+table(data_$Smoker)
+table(data_$CholCheck)
+
+
+pairs.panels(data_ [c("Age", "BMI", "GenHlth", "Fruits")],
+             pch = 21,
+             bg = c("red", "green3")[unclass(data_$Diabetes_012)])
+
