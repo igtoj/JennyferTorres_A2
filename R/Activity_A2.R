f1e5ffd00582bc2e5692011d2bddcf1f37a3c914
igtoj <j.iglandinitorres@gmail.com>
2023-10-09 19:38:43 -0500

    ,,,

--- R/Activity_A2.R
+++ R/Activity_A2.R
@@ -9,7 +9,6 @@ parentFolder <-dirname(folder)
 data <- read.csv(paste0(parentFolder,"/Dataset/diabetes_012.csv"))
 
 data$Diabetes_012 <- ifelse(data$Diabetes_012 == 0, 0, 1)
-
 set.seed(27)
 data_ <- data[sample(nrow(data), 3000), ]
 
